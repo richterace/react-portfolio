e8c2aff631cf17171d5c02f520711302d478d6c5
richterace <villasace098@gmail.com>
2025-07-05 11:12:09 +0800

    hero css

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import styles from './Hero.module.css'
 import { getImageUrl } from '../../utils';
 
 export const Hero = () => {
-    return <section>
+    return <section className={styles.container}>
 
-        <div>
-            <h1>
+        <div className={styles.content}>
+            <h1 className={styles.title}>
                 Hi, I am Ace!
             </h1>
-            <p>
+            <p className={styles.description}>
                 I am a developer and data analyst, reach out if you want to learn more.
             </p>
-            <a href="mailto:villasace098@gmail.com"> Contact Me </a>
+            <a href="mailto:villasace098@gmail.com" className={styles.contactBtn}> Contact Me </a>
         </div>
-        <img src={getImageUrl("hero/heroImage.png")} alt='Hero image of me' />
+        <img src={getImageUrl("hero/heroImage.png")} alt='Hero image of me' className={styles.heroImg}/>
+        <div className={StyleSheet.topBlur}/>
+        <div className={StyleSheet.bottomBlur}/>
     </section>
 };
 
